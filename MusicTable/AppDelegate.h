dc6d630dcde9383fe9ed5515a901423fc7d82b8a
Tom Corcoran <tom@new-host-2.home>
2014-03-10 09:19:33 -0400

    Initial Commit

---
+++ MusicTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MusicTable
+//
+//  Created by Tom Corcoran on 3/10/14.
+//  Copyright (c) 2014 Tom Corcoran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusicTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MusicTable
+//
+//  Created by Tom Corcoran on 3/10/14.
+//  Copyright (c) 2014 Tom Corcoran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
